bed4154a8f366637262fe538d0b9bce72bb24a4d
Danyal <7072946+danyalasif@users.noreply.github.com>
2022-08-15 02:37:59 +0500

    Update packages and get project to run

--- server/app.js
+++ server/app.js
@@ -1,10 +1,8 @@
-import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
 const app = express();
 import session from 'express-session';
-import expressValidator from 'express-validator';
 import mongoose from 'mongoose';
 import logger from 'morgan';
 import passport from 'passport';
@@ -17,9 +15,7 @@ import reviews from './routes/reviews';
 import users from './routes/users';
 import cart from './routes/cart';
 require('dotenv').config();
-// const MongoStore = ConnectMongo(app);
-const MongoStore = require('connect-mongo')(session);
-// 'mongodb://localhost:2000/bythebook'
+
 mongoose.connect(process.env.MONGO_URL).then(
     () => {
         console.log('Mongoose Connection Successful');
@@ -28,8 +24,10 @@ mongoose.connect(process.env.MONGO_URL).then(
         console.log(err);
     }
 );
+
 app.use(cors());
 app.options('*', cors());
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -38,9 +36,9 @@ const staticFiles = express.static(path.join(__dirname, '../../client/build'));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(expressValidator());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(expressValidator());
 app.use(cookieParser());
 app.use(
     session({
@@ -48,7 +46,7 @@ app.use(
         secret: 'keyboard cat',
         resave: false,
         saveUninitialized: false,
-        store: new MongoStore({ mongooseConnection: mongoose.connection })
+        // store: MongoStore.create({ mongoOptions: { m } })
     })
 );
 app.use(passport.initialize());
@@ -62,7 +60,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 //middleware to pass the user info to all routes
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     //make available inside our template
     res.locals.currentUser = req.user;
     //important: move to the code that handles the route
@@ -75,14 +73,14 @@ app.use('/api/reviews', reviews);
 app.use('/api/cart', cart);
 app.use('/*', staticFiles);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
