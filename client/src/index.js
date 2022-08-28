0643caa5568fde541f3f8296b3fcba874bffbea8
Danyal <7072946+danyalasif@users.noreply.github.com>
2022-08-28 21:26:11 +0500

    Update redux devtools

--- client/src/index.js
+++ client/src/index.js
@@ -14,7 +14,7 @@ import { createStore, applyMiddleware } from 'redux';
 
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 import rootReducer from './rootReducer';
 
